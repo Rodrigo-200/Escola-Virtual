fdd08b653d4377335ef162cd6f97070e24475e7c
Rodrigo-200 <84940356+Rodrigo-200@users.noreply.github.com>
2024-10-09 17:09:49 +0100

    Simple Changes

--- Escola Virtual/Class.cs
+++ Escola Virtual/Class.cs
@@ -19,6 +19,8 @@ namespace Escola_Virtual
         private string _class_name;
         private string _class_acronym;
 
+        //testee
+
 
         #region Propriedades de Get
         public List<Student> Get_List_Of_Student
